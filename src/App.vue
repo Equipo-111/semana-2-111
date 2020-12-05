42c723e454b95396da929621192259036a9914aa
Omar González Reyes <omargreyes@hotmail.es>
2020-12-05 18:30:30 -0500

    firts commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    
+    <page-footer></page-footer>
+    <h1>{{title}}</h1>
+    
+  </div>
+</template>
+
+<script>
+
+import PageFooter from './components/PageFooter.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return{
+      title: 'Vivo dentro de Vue'
+    }
+  },
+  components: {
+   PageFooter
+  }
+}
+</script>
+
